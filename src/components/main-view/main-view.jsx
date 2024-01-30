010d1c092928b1c5648ed726260413f9fecbc79b
Caduella <146942933+Caduella@users.noreply.github.com>
2024-01-30 19:33:06 +0100

    Populate the movie list from render API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,63 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Inception",
-      image:
-        "https://i.ebayimg.com/thumbs/images/g/~ygAAOSwbV5ltN5k/s-l300.webp",
-      description: "A skilled thief, the absolute best in the dangerous art of extraction, stealing valuable secrets from deep within the subconscious during the dream state, is given a chance at redemption.",
-      genre: "Science Fiction",
-      director: "Christopher Nolan",
-      featured: "No"
-    },
-    {
-      id: 2,
-      title: "The Shawshank Redemption",
-      image:
-        "https://i.ebayimg.com/images/g/JEwAAOSwOrxga~Bd/s-l960.webp",
-      description:"Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-      genre: "Drama",
-      director: "Frank Darabont",
-      featured: "Yes"
-    },
-    {
-      id: 3,
-      title: "The Fountain",
-      image:
-        "https://i.ebayimg.com/images/g/Z1cAAOSwysBghuS8/s-l960.webp",
-      description:"As a modern-day scientist, Tommy is strugging with mortality, desperately searching for the medical breakthrough that will save the life of his cancer-stricken wife, Izzi.",
-      genre: "Drama",
-      director: "Darren Aronofsky",
-      featured: "Yes"
-    },
-    {
-      id: 4,
-      title: "Forrest Gump",
-      image:
-        "https://i.ebayimg.com/images/g/iU4AAOSwxMlezexq/s-l960.webp",
-      description:"The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
-      genre: "Drama",
-      director: "Robert Zemeckis",
-      featured: "No"
-    },
-    {
-      id: 5,
-      title: "Spirited Away",
-      image:
-        "https://i.ebayimg.com/images/g/8yIAAOSwlKhghuOu/s-l960.webp",
-      description:"This animated fantasy film tells the story of Chihiro, a sullen ten-year-old girl who, while moving to a new neighborhood, enters the world of Kami (spirits) of Japanese Shinto folklore. She must find the courage and resolve to free herself and her parents from the spell of the witch Yubaba.",
-      genre: "Animation",
-      director: "Hayao Miyazaki",
-      featured: "Yes"
-    },
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
-	
+ 
+  useEffect(() => {
+    fetch("https://myquickmovieapi.onrender.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const moviesFromApi = data.map((movie) => {
+          return {
+            _id: movie._id,
+            Title: movie.Title,
+            Description: movie.Description,
+            Genre: {
+              GenreName: movie.Genre.Name,
+              GenreDescription: movie.Genre.Description
+            },
+            Director: {
+              DirectorName: movie.Director.Name,
+              DirectorBio: movie.Director.Bio,
+              DirectorBirth: movie.Director.Birth,
+              DirectorDeath: movie.Director.Death
+            },
+            ImagPath: movie.ImagePath,
+            Featured: movie.Featured
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
+ 
+
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
@@ -71,7 +50,7 @@ export const MainView = () => {
     <div>
         {movies.map((movie) => (
           <MovieCard
-            key={movie.id}
+            key={movie._id}
             movie={movie}
             onMovieClick={(newSelectedMovie) => {
               setSelectedMovie(newSelectedMovie);
